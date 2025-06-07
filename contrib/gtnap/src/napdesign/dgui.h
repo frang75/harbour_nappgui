4aee47b775232d6452a0cca027ef10453afc88af
Francisco García <frang@nappgui.com>
2025-06-07 15:34:32 +0200

    [GTNAP-528] wip: Implementing drawers

--- contrib/gtnap/src/napdesign/dgui.h
+++ contrib/gtnap/src/napdesign/dgui.h
@@ -5,3 +5,5 @@
 void dgui_init(void);
 
 View *dgui_panel_header(const char_t *title, const Font *font, Listener *OnClose);
+
+Panel *dgui_drawer(const char_t *title, const Font *font, Panel *child, const bool_t open);
