0af2fc10e57898c2ea400ad234d939e8d28c7a72
Francisco García Collado <42999199+frang75@users.noreply.github.com>
2026-07-04 11:11:27 +0200

    [GTNAP-657] wip: Added AWS include files to HBAWS

---
+++ contrib/hbaws/awssdk/include/aws/crt/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#define AWS_CRT_CPP_VERSION "0.34.2"
+#define AWS_CRT_CPP_VERSION_MAJOR 0
+#define AWS_CRT_CPP_VERSION_MINOR 34
+#define AWS_CRT_CPP_VERSION_PATCH 2
+#define AWS_CRT_CPP_GIT_HASH "fbe8fa52486c0a7a3aa8b1998a5b175d15ad2cac"
